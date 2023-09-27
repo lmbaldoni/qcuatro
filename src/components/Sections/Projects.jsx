dab40ed64af68d7a4063693865e8107a956f93b5
Lucas <lmbaldoni@gmail.com>
2023-09-27 20:19:26 +0200

    saas imagenes

--- src/components/Sections/Projects.jsx
+++ src/components/Sections/Projects.jsx
@@ -4,12 +4,12 @@ import styled from "styled-components";
 import ProjectBox from "../Elements/ProjectBox";
 import FullButton from "../Buttons/FullButton";
 // Assets
-import ProjectImg1 from "../../assets/img/projects/1.png";
-import ProjectImg2 from "../../assets/img/projects/2.png";
-import ProjectImg3 from "../../assets/img/projects/3.png";
-import ProjectImg4 from "../../assets/img/projects/4.png";
-import ProjectImg5 from "../../assets/img/projects/5.png";
-import ProjectImg6 from "../../assets/img/projects/6.png";
+import ProjectImg1 from "../../assets/img/saas/1.png";
+import ProjectImg2 from "../../assets/img/saas/2.png";
+import ProjectImg3 from "../../assets/img/saas/3.png";
+import ProjectImg4 from "../../assets/img/saas/4.png";
+import ProjectImg5 from "../../assets/img/saas/5.png";
+import ProjectImg6 from "../../assets/img/saas/6.png";
 import AddImage2 from "../../assets/img/add/add2.png";
 
 export default function Projects() {
